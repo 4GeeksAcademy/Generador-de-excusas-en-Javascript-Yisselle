445ce98c539b1cb62e1a43dd49f90ff8065ff046
NavarreteYisselle <yiselle.navarrete21@gmail.com>
2026-04-14 02:14:45 +0000

    temlate

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [' Mi perro ', ' Mi abuelita ', ' Tu tio ', ' mi pana '];
+  let action = [' corrio ', ' se cayo ', ' rompio ', ' averio '];
+  let what = [' mi tarea ', ' mi celular ', ' mi nave '];
+  let when = [' antes de la clase ', ' Al salir de casa ', ' mientras dormia ', ' cuando saltaba en un pie ', ' cuando comenzo el apocalipsis zombie '];
+  
+  function getRandomElement (arr){
+    return arr[Math.floor(Math.random() * arr.length)]
+  }
+  let excuse = getRandomElement(who) +""+ getRandomElement(action) +""+ getRandomElement(what) +""+ getRandomElement(when);
+  console.log(excuse);
+  document.getElementById('excuse').innerHTML = excuse;
 };
